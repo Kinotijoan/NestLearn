1643e60691deaa7bb8f17aff4815add3b613f68d
Kinotijoan <kinotijoan@gmail.com>
2024-11-02 11:02:20 +0300

    Refine NavBar styling
    Updated the NavBar with a light blue background and restyled the "Get started" button with a gold background and a hover effect that adds a gold border.  This enhances visual appeal and improves user experience.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,7 @@ export default function NavBar() {
 
 
         <nav
-            className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            className="bg-[#bfe1fe] h-[90px] dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <span
@@ -11,7 +11,7 @@ export default function NavBar() {
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                     <button type="button"
-                            className="text-black bg-[#bfe1fe] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
+                            className="text-black bg-[#FCD980]  hover:bg-transparent  hover:outline-none border-2 border-transparent hover:border-[#FCD980] font-medium rounded-[5px] text-sm px-4 py-2 text-center">Get
                         started
                     </button>
                     <button data-collapse-toggle="navbar-sticky" type="button"
@@ -20,14 +20,14 @@ export default function NavBar() {
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                              viewBox="0 0 17 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2"
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                   d="M1 1h15M1 7h15M1 13h15"/>
                         </svg>
                     </button>
                 </div>
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                      id="navbar-sticky">
-                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-[#bfe1fe] dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a href="#"
                                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
